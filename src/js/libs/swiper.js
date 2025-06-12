7966930043e45e8ff51dfab4bd28a894dcf5bd14
Anatoly <povly19995@gmail.com>
2025-06-12 09:29:53 +0300

    [refactor] Clean up unused files and simplify header structure
    - Deleted about.html, about.scss, contacts.scss, first.scss, projects.scss, smi.scss, thing.scss, and their associated styles to reduce clutter.
    - Commented out Swiper import statements in swiper.js to prevent unused code execution.
    - Refactored header.pug to streamline structure and improve readability, consolidating elements into a more organized layout.
    - Updated index.scss to remove references to deleted SCSS files, ensuring a cleaner import structure.

--- src/js/libs/swiper.js
+++ src/js/libs/swiper.js
@@ -1,2 +1,2 @@
-import Swiper from '../../../modules/swiper-master/dist/swiper-bundle.mjs';
-window.Swiper = Swiper;
+// import Swiper from '../../../modules/swiper-master/dist/swiper-bundle.mjs';
+// window.Swiper = Swiper;
